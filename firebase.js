29ae3134e1864d278fcf81e49e23770d76138645
melek <melekhedhili7@gmail.com>
2022-09-08 20:14:42 +0100

    'updated'

--- firebase.js
+++ firebase.js
@@ -16,16 +16,15 @@ import {getFirestore, initializeFirestore} from 'firebase/firestore';
 //import firebase realtime database from firebase
 
 const firebaseConfig = {
-  apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
-  databaseURL: DB_URL,
-  projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: MESSAGING_SENDER_ID,
-  appId: APP_ID,
-  measurementId: MESUREMENT_ID,
+  apiKey: 'AIzaSyDhCcKHroheMhN7jJrm_kXtyL5fkfvx2p8',
+  authDomain: 'tinder-clone-4df13.firebaseapp.com',
+  databaseURL: 'https://tinder-clone-4df13-default-rtdb.firebaseio.com',
+  projectId: 'tinder-clone-4df13',
+  storageBucket: 'tinder-clone-4df13.appspot.com',
+  messagingSenderId: '17526967147',
+  appId: '1:17526967147:web:3f0bd95cbde5086a621a2e',
+  measurementId: 'G-7EWXF09ZV2',
 };
-
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = initializeFirestore(app, {
